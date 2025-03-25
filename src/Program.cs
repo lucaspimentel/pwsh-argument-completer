ee3a616991521b87148a40452b1874b95066410f
Lucas Pimentel <lucaspimentel@gmail.com>
2025-03-25 16:21:48 -0400

    initial code commit

---
+++ src/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace PowerShellArgumentCompleter;
+
+// https://learn.microsoft.com/en-us/powershell/scripting/learn/shell/tab-completion
+// https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/register-argumentcompleter
+// https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/tabexpansion2
+// https://techcommunity.microsoft.com/blog/itopstalkblog/autocomplete-in-powershell/2604524
+// https://github.com/abgox/PSCompletions/blob/main/completions/scoop/language/en-US.json
+
+
+/*
+$scriptblock = {
+    param(
+        $wordToComplete,
+        $commandAst,
+        $cursorPosition
+    )
+
+dotnet complete --position $cursorPosition $commandAst.ToString() | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new(
+            $_,               # completionText
+            $_,               # listItemText
+            'ParameterValue', # resultType
+            $_                # toolTip
+        )
+    }
+}
+
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock $scriptblock
+*/
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+    }
+}
